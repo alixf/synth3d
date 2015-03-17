3d11689a0749a4008515c9cdb0a75415a941e105
Baptiste FUMAROLI <baptiste.fumaroli@gmail.com>
2015-03-17 18:59:07 +0100

    Now creates an oscillator and a gain.

--- src/blocks/WaveBlock.js
+++ src/blocks/WaveBlock.js
@@ -3,10 +3,14 @@ window.WaveBlock = function()
     var geometry	= new THREE.BoxGeometry(1,1,1);
     var material	= new THREE.MeshLambertMaterial({map : Textures.wave}); 
     var mesh	    = new THREE.Mesh(geometry, material);
+    mesh.audioNode = audioCtx.createOscillator();
     mesh.position.y = 0.5;
     mesh.castShadow = true;
     mesh.receiveShadow = true;
     mesh.transformable = true;
     mesh.blockType = "wave";
+    mesh.audioNodeGain = audioCtx.createGain();
+    mesh.audioNode.connect(mesh.audioNodeGain);
+    mesh.audioNodeGain.connect(audioCtx.destination);
     return mesh;
 };
